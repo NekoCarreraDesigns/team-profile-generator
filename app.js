e0075eb35e49340d7d9dceca8d1041432ef53330
NekoCarreraDesigns <denvermaas@gmail.com>
2020-06-09 23:50:09 -0600

    rewrite for the team generator homework

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const Employee = require("../lib/Employee");
-const Manager = require("../lib/Manager");
-const Engineer = require("../lib/Engineer");
-const Intern = require("../lib/Intern");
+//const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -9,93 +9,150 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./output/htmlRenderer");
+const render = require("./lib/htmlRenderer");
 
-const employeeData = [];
+let employeeData = [];
 
-
-inquirer.prompt([{
-
-    type: "input",
-    name: "name",
-    message: "What is your name?"
-},
-{
-    type: "input",
-    name: "role",
-    message: "What is your role?"
-},
-{
-    type: "input",
-    name: "id",
-    message: "What is your id?"
-},
-{
-    type: "input",
-    name: "email",
-    message: "What is your email?"
-},
-{
-    type: "input",
-    name: "officeNumber",
-    message: "what is your office number?"
-},
-{
-    type: "list",
-    name: "employees",
-    message: "Would you like to add more employees?",
-    choices: [
-        "yes",
-        "no"
-    ]
-
-},
-
-]).then((res) => {
-    const managerData = new Manager("res.name, res.id, res.role, res.officeNumber, res.email")
-    employeeData.push(managerData)
-
-    if (res.employees === "yes") {
-        inquirer.prompt([{
-            type: "list",
-            name: "employee",
-            message: "What is the employees position?",
-            choices: [
-                "Engineer",
-                "Intern"
-            ]
-
-        }])
-    } else if (res.employees === "Intern") {
-        inquirer.prompt([{
-            type: "input",
-            name: "intern",
-            message: "What school do they attend?"
-        }])
-    } else if (res.employee === "Engineer") {
-        inquirer.prompt([{
-            type: "input",
-            name: "engineer",
-            message: "What is their github username?"
-        }])
-    } else {
-        alert("Not a fan of teamwork")
-        console.log(alert)
+let html;
+const managerPrompt = async () => {
+    try {
+        const { name, id, email, officeNumber } = await prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "what is you name?"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is your Id?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is your email address?"
+            },
+            {
+                type: "input",
+                name: "officeNumber",
+                message: "What is your office number?"
+            },
+        ]);
+        employeeData.push(new Manager(name, id, email, officeNumber));
+    } catch (err) {
+        throw err;
     }
-
-    const employee = new Employee(`res.name, res.email, res.role, res.id`)
-    employeeData.push(employeeData)
-    const engineer = new Engineer(`res.github, ${employee}`)
-    employeeData.push(engineer)
-    const intern = new Intern(`res.school, ${employee}`)
-    employeeData.push(intern)
-
-
-})
-
-fs.writeFile("team.html", "utf-8", (err) => {
-    if (err) {
-        throw err
+};
+const internPrompt = async () => {
+    try {
+        const { name, id, email, school } = await prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "What is their name?"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is their Id?",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is their email address?"
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "Where do they attend school?"
+            },
+        ]);
+        employeeData.push(new Intern(name, id, email, school));
+    } catch (err) {
+        throw err;
     }
-    console.log(err)
-})
+};
+
+const engineerPrompt = async () => {
+    try {
+        const { name, id, email, github } = await prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "What is their name?",
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is their Id?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is their email address?"
+            },
+            {
+                type: "input",
+                name: "github",
+                message: "What is their Github username?"
+            },
+        ]);
+        employeeData.push(new Engineer(name, id, email, github));
+    } catch (err) {
+        throw err;
+    }
+};
+
+
+const mainPrompt = async () => {
+    try {
+        const { next } = await prompt([
+            {
+                name: "next",
+                message: "What team member would you like to add?:",
+                type: "list",
+                choices: ["Intern", "Engineer", "I'm finished"],
+            },
+        ]);
+
+        switch (next) {
+            case "I'm finished":
+                console.log(employees);
+                html = render(employees);
+                return;
+            case "Intern":
+                await internPrompt();
+                await mainPrompt();
+                return;
+            case "Engineer":
+                await engineerPrompt();
+                await mainPrompt();
+                return;
+        }
+    } catch (err) {
+        throw err;
+    }
+};
+
+const write = async () => {
+    try {
+        await fs.mkdirSync(OUTPUT_DIR, (err) => {
+            throw err;
+        });
+        await fs.writeFileSync(outputPath, html, (err) => {
+            throw err;
+        });
+    } catch (err) {
+        throw err;
+    }
+};
+
+const init = async (html) => {
+    try {
+        await mainPrompt();
+        await write(html);
+    } catch (err) {
+        throw err;
+    }
+};
+init();
